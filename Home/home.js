9a0dfb3515f9af945932a48dd37028a390821892
KaltrinaKrasniqi <116883212+KaltrinaKrasniqi@users.noreply.github.com>
2024-01-11 22:34:46 +0100

    Update home.js

--- Home/home.js
+++ Home/home.js
@@ -7,3 +7,18 @@ function myMenuFunction() {
         i.className = "nav-menu";
     }
    }
+
+   $(document).ready(function () {
+    
+    // Vendos një event për kur mausi largohet nga butoni
+    $(".btn").mouseenter(function () {
+      
+      $(this).css("width", "10rem");
+      $(this).css("height", "5rem");
+    });
+
+    $(".btn").mouseleave(function () {
+    // Kthe madhësinë e butonit në vlerën fillestare
+    $(this).css("width", "8rem");
+    $(this).css("height", "auto");
+   });
