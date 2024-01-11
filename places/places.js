9418232baab3ec696d6a11f8aa94fd45616c69e5
KaltrinaKrasniqi <116883212+KaltrinaKrasniqi@users.noreply.github.com>
2024-01-11 23:01:24 +0100

    Update places.js

--- places/places.js
+++ places/places.js
@@ -8,3 +8,13 @@ $(document).ready(function(){
           $("input#btn1").hide(500);
           $("input#btn12").show(1000);
       })
+      $("input#btn12").on('click', ()=>{
+        $('p#par1').hide(500);
+        $("input#btn1").show(1000);
+        $("input#btn12").hide(500);
+    });
+
+
+    
+  });
+});
